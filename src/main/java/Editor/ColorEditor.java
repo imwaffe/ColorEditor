d6daaff3f7b3cbc8389bf8199b03149b8db17c95
Luca <iz2zzs.luca@gmail.com>
2021-01-30 19:34:35 +0100

    high resolution histograms

--- src/main/java/Editor/ColorEditor.java
+++ src/main/java/Editor/ColorEditor.java
@@ -21,7 +21,7 @@ public class ColorEditor {
         AlterColor alterColor = new AlterRGB();
         ImageProxy imageProxy = new ImageProxy(alterColor);
         GUIObserver gui = new GUIObserver(imageProxy);
-        FileController fileController = new MockFileController(gui, imageProxy);
+        FileController fileController = new FileChooserController(gui, imageProxy);
         KeyboardController keyboardController = new KeyboardController(KeyboardFocusManager.getCurrentKeyboardFocusManager());
         HistogramController histogram = new HistogramController(gui.getRightPanel(),imageProxy,true);
 
